59004c75f8932dca66ea3e83c2fe66ad45278dd4
neki-dev <dev@neki.guru>
2022-03-28 05:34:12 +0300

    Added linter & Minor fixes

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@neki-dev/eslint-config'],
+};
--- index.js
+++ index.js
@@ -4,40 +4,40 @@
  * @param {Array} current - Current array
  * @param {Array} target - Target array
  * @param {Function} match - Function of matching arrays items
- * 
+ *
  * @return {Object} api
  */
-module.exports = async function (current, target, match) {
-	const result = {
-		excess: [],
-		match: [],
-		missing: [],
-	};
-  
-	for (const currentItem of current) {
-		if (target.every((targetItem) => !match(targetItem, currentItem))) {
-			result.excess.push([currentItem]);
-		}
-	}
-	for (const targetItem of target) {
-		const currentItem = current.find((currentItem) => match(targetItem, currentItem));
-		if (currentItem) {
-			result.match.push([targetItem, currentItem]);
-		} else {
-			result.missing.push([targetItem]);
-	  	}
-	}
+module.exports = async function contrast(current, target, match) {
+  const result = {
+    excess: [],
+    match: [],
+    missing: [],
+  };
 
-	const api = {};
-	for (const [type, items] of Object.entries(result)) {
-		api[type] = async (callback) => {
-			for (const args of items) {
-				await callback(...args);
-			}
-			return api;
-		}
-	}
+  current.forEach((currentItem) => {
+    if (target.every((targetItem) => !match(targetItem, currentItem))) {
+      result.excess.push([currentItem]);
+    }
+  });
 
-	return api;
+  target.forEach((targetItem) => {
+    const sameItem = current.find((currentItem) => match(targetItem, currentItem));
+    if (sameItem) {
+      result.match.push([targetItem, sameItem]);
+    } else {
+      result.missing.push([targetItem]);
+    }
+  });
+
+  const api = {};
+  Object.entries(result).forEach(([type, items]) => {
+    api[type] = (callback) => {
+      items.forEach(async (args) => {
+        await callback(...args);
+      });
+      return api;
+    };
+  });
+
+  return api;
 };
-  
